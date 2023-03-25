e99f3d343ad22208c13780bff16af34805b5bcd5
codesari <ahmtsr81@gmail.com>
2023-03-26 01:37:26 +0300

    class-based views in templates

--- todo/forms.py
+++ todo/forms.py
@@ -3,6 +3,8 @@ from .models import Todo
 # bu yapı serializers gibi
 class TodoForm(forms.ModelForm):
     class Meta:
-        model:Todo
+        model=Todo
+        # fields="__all__"
         exclude=[]
         # çıkartmak istediğin fieldları yaz
+        
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
 from .models import Todo
+from .forms import TodoForm
+from django.urls import reverse_lazy
 
 
-def todo_list(request):
-    todos=Todo.objects.all()
-    context = {'todos': todos}
-    return render(request, 'list.html', context)
+class TodoListView(ListView):
+    model=Todo
+    template_name = 'todo/list.html'
+
+# ? create ve update işlemlerinde form gerekli oduğundan form_class=TodoForm satırını bu fonksiyonlarda çağırdık
+class TodoCreateView(CreateView):
+    model=Todo
+    form_class=TodoForm
+    template_name = 'todo/add.html'
+    # todo/todo_form.html
+    success_url=reverse_lazy('todo_list')
+    
+class TodoUpdateView(UpdateView):
+    model = Todo
+    form_class = TodoForm
+    template_name = 'todo/update.html'
+    # todo/todo_update.html
+    success_url = reverse_lazy('todo_list')
+    
+class TodoDeleteView(DeleteView):
+    model=Todo
+    template_name = 'todo/delete.html'
+    # todo/todo_confirm_delete.html
+    success_url = reverse_lazy('todo_list')
+class TodoDetailView(DetailView):
+    model = Todo
+    template_name = 'todo/detail.html'
+    # todo/todo_detail.html
+    success_url = reverse_lazy('todo_list')
+        
